9b4bb85fdb4cb6828814bb3c44d6529b820ef1d0
Гафиятуллин Дамир <damir.gafiyatullin@akbarsdigital.ru>
2021-04-27 18:56:45 +0300

    Добавлен web сервер

--- itis/cmd/itis/main.go
+++ itis/cmd/itis/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"os"
+)
 
+func main() {
+	logrus.Info("Hello world")
+
+	port := os.Getenv("PORT")
+
+	if len(port) == 0 {
+		logrus.Fatal("Порт не указан")
+	}
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("This is a simple page"))
+	})
+
+	http.ListenAndServe(":" + port, nil)
 }
